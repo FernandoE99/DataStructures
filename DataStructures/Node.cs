fa745f08e6b5b4e2de0eb91ee7eafa12f2c4ec38
Fernando <ferescoto98@gmail.com>
2020-04-17 22:05:23 -0700

    Added DataStructure classes

---
+++ DataStructures/Node.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Node
+    {
+
+        private object data;
+        Node next;
+
+
+        Node()
+        {
+            next = this;
+        }
+
+       public Node(object data, Node Next)
+        {
+            this.data = data;
+            this.next = this;
+        }
+
+        object information()
+        {
+            return data;
+        }
+
+        Node next()
+        {
+            return next;
+        }
+
+    }
+}
