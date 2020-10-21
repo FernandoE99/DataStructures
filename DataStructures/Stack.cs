b09fe545e776e52c0f247897e93a2da46f0094b0
Fernando <ferescoto98@gmail.com>
2020-10-20 23:33:35 -0700

    Added new Data Structure
    Added the Queue data structure.

--- DataStructures/Stack.cs
+++ DataStructures/Stack.cs
@@ -4,7 +4,38 @@ using System.Text;
 
 namespace DataStructures
 {
-    class Stack
+    class Stack<T>
     {
+        private Node<T> head;
+
+        public void push(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        public T pop() {
+            if (head == null) { throw new ArgumentNullException("There is no value to Pop"); }
+            T data = head.Data;
+            head = head.Next;
+            return data;
+        }
+
+        public T peek() { 
+            if(head == null) { throw new ArgumentNullException("There is no value to peek"); }
+            return head.Data;
+        }
+
+        public bool isEmpty() {
+            return head == null;
+        }
+
+        public void clear() { 
+            if(isEmpty()) { throw new ArgumentNullException("There is no values to clear"); }
+            while (!isEmpty()) {
+                head = head.Next;
+            }
+        }
     }
 }
